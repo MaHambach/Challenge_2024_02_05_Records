e84e006b4483cf7757970946976b353d84358b76
mahambach <ma.hambach@gmx.net>
2024-02-05 10:51:39 +0100

    Initial commit

---
+++ src/main/java/org/github/mahambach/Main.java
@@ -0,0 +1,9 @@
+package org.github.mahambach;
+
+public class Main {
+    public static void main(String[] args) {
+        double a = 0.3;
+        double b = 0.3;
+        System.out.println(0.3+0.3+0.3);
+    }
+}
---
+++ src/main/java/org/github/mahambach/Records/Animal.java
@@ -0,0 +1,8 @@
+package org.github.mahambach.Records;
+
+public record Animal(
+        int ID,
+        String Name,
+        String Species,
+        int Age) {
+}
